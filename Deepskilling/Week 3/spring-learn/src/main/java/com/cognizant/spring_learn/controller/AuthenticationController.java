7d98fa3056bce1c3f42c6230f261b7b901387abf
Sujith Roy <sujithroylingam@gmail.com>
2026-07-10 16:14:09 +0530

    Files uploaded

---
+++ Deepskilling/Week 3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package com.cognizant.spring_learn.controller;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.model.AuthenticationResponse;
+import com.cognizant.spring_learn.util.JwtUtil;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/authenticate")
+    public AuthenticationResponse authenticate(
+            @RequestHeader("Authorization") String authorizationHeader) {
+
+        String encodedCredentials = authorizationHeader.substring(6);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedCredentials);
+
+        String credentials = new String(decodedBytes, StandardCharsets.UTF_8);
+
+        String username = credentials.split(":")[0];
+
+        String token = jwtUtil.generateToken(username);
+
+        return new AuthenticationResponse(token);
+    }
+}
---
+++ Deepskilling/Week 3/spring-learn/src/main/java/com/cognizant/spring_learn/util/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.cognizant.spring_learn.util;
+
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET =
+            "mysecretkeymysecretkeymysecretkey123";
+
+    public String generateToken(String username) {
+
+        Key key = new SecretKeySpec(
+                SECRET.getBytes(),
+                SignatureAlgorithm.HS256.getJcaName());
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(key)
+                .compact();
+    }
+}
